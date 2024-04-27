f8aa425d412bfe862ffcb39f1bf61cd4afd0e898
IMRANDIL <aliimranadil2@gmail.com>
2024-04-27 08:28:52 +0530

    some more refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 // App.js
-import { useState } from 'react';
+
 import './App.css';
 import Board from './components/Board';
-import Square from './components/Square';
+
 
 function App() {
-  const [squares, setSquares] = useState(Array(9).fill(null)); // Array to hold square values
-
-  const handleClick = (index) => {
-    const newSquares = squares.slice(); // Create a copy of the squares array
-    newSquares[index] = 'X'; // Set the value at the clicked index to 'X'
-    setSquares(newSquares); // Update the state with the new squares array
-  };
+;
 
   return (
     <div className='container'>
       <h1>Tic Tac Toe</h1>
-      <Board>
-        {squares.map((value, index) => (
-          <Square key={index} value={value} onClick={() => handleClick(index)} />
-        ))}
-      </Board>
+      <Board/>
     </div>
   );
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,24 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import Square from './Square';
 
-const Board = ({ children }: { children: ReactNode }) => {
+const Board = () => {
+    const [squares, setSquares] = useState(Array(9).fill(null)); // Array to hold square values
+
+    const handleClick = (index) => {
+      const newSquares = squares.slice(); // Create a copy of the squares array
+      newSquares[index] = 'X'; // Set the value at the clicked index to 'X'
+      setSquares(newSquares); // Update the state with the new squares array
+    };
     
   return (
-    <div className='board'>{children}</div>
+    <>
+    <div className='board'>
+    {squares.map((value, index) => (
+          <Square key={index} value={value} onClick={() => handleClick(index)} />
+        ))}
+    </div>
+    </>
+    
   );
 };
 
