42da49412befb2cbeb63ff36aa7755628b8904b0
IMRANDIL <aliimranadil2@gmail.com>
2024-04-27 08:44:58 +0530

    some more changes done

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,13 +3,19 @@ import Square from './Square';
 
 const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null)); // Array to hold square values
-
+    const [xIsNext, setXIsNext] = useState(true);
     const handleClick = (index) => {
       const newSquares = squares.slice(); // Create a copy of the squares array
-      newSquares[index] = 'X'; // Set the value at the clicked index to 'X'
-      setSquares(newSquares); // Update the state with the new squares array
-    };
+      if (xIsNext) {
+        newSquares[index] = "X";
+      } else {
+        newSquares[index] = "O";
+      }
     
+      setSquares(newSquares); // Update the state with the new squares array
+      setXIsNext(!xIsNext);
+    };
+    console.log(squares)
   return (
     <>
     <div className='board'>
