9199c6d4b82ed5877982f18634c216447cbb58eb
IMRANDIL <aliimranadil2@gmail.com>
2024-04-27 09:58:59 +0530

    learning react the documentation way

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,17 +1,33 @@
+// import { useEffect, useState } from "react";
 import { useTicTacToe } from "../customhooks/useTicTacToe ";
 import Square from "./Square";
 
 const Board = () => {
     const { squares, handleClick, winner, scores, handleRestart } = useTicTacToe();
-  
-    let status;
+    // const [status, setStatus] = useState("Current player: X")
+  let status;
     if (winner) {
+        // setStatus("Winner: " + winner)
       status = "Winner: " + winner;
     } else if (squares.every((square) => square !== null)) {
+        // setStatus("It's a draw!")
       status = "It's a draw!";
     } else {
-      status = "Next player: " + (squares.filter((square) => square !== null).length % 2 === 0 ? "X" : "O");
+         status = squares.some((square) => square !== null) ? "Next player: " : "Current player: " + (squares.filter((square) => square !== null).length % 2 === 0 ? "X" : "O");
+        // setStatus(statustoUpdate)
     }
+
+       // Update status whenever there's a change in the game state
+    //    useEffect(() => {
+    //     if (winner) {
+    //         setStatus("Winner: " + winner);
+    //     } else if (squares.every((square) => square !== null)) {
+    //         setStatus("It's a draw!");
+    //     } else {
+    //         let statusToUpdate = squares.some((square) => square !== null) ? "Next player: " : "Current player: " + (squares.filter((square) => square !== null).length % 2 === 0 ? "X" : "O");
+    //         setStatus(statusToUpdate);
+    //     }
+    // }, [squares, winner]);
   
     return (
       <>
@@ -21,7 +37,7 @@ const Board = () => {
             marginBottom: "10px",
           }}
         >
-          {status}
+          {status && status}
         </div>
         <div className="board">
           {squares.map((value, index) => (
@@ -32,7 +48,7 @@ const Board = () => {
             />
           ))}
         </div>
-        <button onClick={handleRestart}>Restart</button>
+        <button className="reset" onClick={handleRestart}>Restart</button>
         <div className="scores">
           <p>X: {scores.X}</p>
           <p>O: {scores.O}</p>
